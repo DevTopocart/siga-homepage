f194c60c2896b6c3031bd66c679b9ff55761fbda
Victor Marotta <victordossantosmarotta@gmail.com>
2024-04-23 15:47:29 -0300

    refact: refatora obtencao de dados da API do SIGA passando pelo proxy da API Intranet

--- Js/new-painel-servicos.js
+++ Js/new-painel-servicos.js
@@ -32,52 +32,75 @@ $("#botao-servico-sigweb").hover(
   () => {}
 );
 
-async function sendRequestAndOpenTab() {
-  const extensionSelector = document.querySelector("#file-extension-select");
-  const filenameSelector = document.querySelector("#file-name-select");
+const apiUrlIntranet = `https://api.intranet.topocart.dev.br`
 
-  const extension = extensionSelector.value;
-  const filename = filenameSelector.value;
-
-  const response = await fetch("https://siga.angra.rj.gov.br/portal/camadas", {
-    headers: {
-      "API-key":
-        "AAAAB3NzaC1yc2EAAAADAQABAAABAQCUoDtVtIK52TpuQqPmYL+/SgndGTDG5OObi7o1lShhn3Pf1lAtRKOKrsYPJXmSv34/tuA7t+iGlNUL6h6hQr9HOtuKj1b8JpFrBIjlkcIabl/rT9ndmpDgC9a1oTsin6y6ifNvl2tPvHWPzgxDa4Z3hGGxZeFPocplN/Up0CARg1NZrbNb+1YCJeoMRka//d+bJjxO6ndgpJ7Hhukrp/dqgl/+Hfv6ID2wKE0+019cu8wmv+zVGAe7ctY8jPwaa8qGWdwF2lGPTurH2I//jjtxPQrmscR88xVNO68Jr5GY0Rfih3MR+NnVdc5GcvPkGjutHHrv4+wOz+MKUkj4xOIf",
-    },
-  });
-
-  const data = await response.json();
-
-  data.forEach((item) => {
-    if (item.extension === extension && item.filename === filename) {
-      window.open(item.url, "_blank");
-    }
-  });
-}
-
-async function sendRequestAndOpenTabs() {
-  const healthUnitSelector = document.querySelector("#mapa-filename-select");
-
-  const selectedOption = healthUnitSelector.value;
-
-  const response = await fetch(
-    "https://siga.angra.rj.gov.br/portal/mapas_tematicos",
-    {
+async function getListOfCamadas() {
+  try {
+    
+    const request = await fetch(apiUrlIntranet + "/gateway/siga?endpoint=/portal/camadas", {
       headers: {
+        "User-Agent": "siga-homepage",
         "API-key":
           "AAAAB3NzaC1yc2EAAAADAQABAAABAQCUoDtVtIK52TpuQqPmYL+/SgndGTDG5OObi7o1lShhn3Pf1lAtRKOKrsYPJXmSv34/tuA7t+iGlNUL6h6hQr9HOtuKj1b8JpFrBIjlkcIabl/rT9ndmpDgC9a1oTsin6y6ifNvl2tPvHWPzgxDa4Z3hGGxZeFPocplN/Up0CARg1NZrbNb+1YCJeoMRka//d+bJjxO6ndgpJ7Hhukrp/dqgl/+Hfv6ID2wKE0+019cu8wmv+zVGAe7ctY8jPwaa8qGWdwF2lGPTurH2I//jjtxPQrmscR88xVNO68Jr5GY0Rfih3MR+NnVdc5GcvPkGjutHHrv4+wOz+MKUkj4xOIf",
       },
-    }
-  );
-
-  const data = await response.json();
-
-  data.forEach((item) => {
-    const wordsInOption = selectedOption.split(" ");
-    const wordsInFilename = item.filename.split(" ");
-
-    if (wordsInOption.every((word) => wordsInFilename.includes(word))) {
-      window.open(item.url, "_blank");
-    }
-  });
+    });
+  
+    return request.json()
+  } catch (error) {
+    
+    console.warn('Não foi possivel obter a lista de camadas', error)
+  }
 }
+
+async function getListOfMapasTematicos() {
+  try {
+    
+    const request = await fetch(apiUrlIntranet + "/gateway/siga?endpoint=/portal/mapas_tematicos", {
+      headers: {
+        "User-Agent": "siga-homepage",
+        "API-key":
+          "AAAAB3NzaC1yc2EAAAADAQABAAABAQCUoDtVtIK52TpuQqPmYL+/SgndGTDG5OObi7o1lShhn3Pf1lAtRKOKrsYPJXmSv34/tuA7t+iGlNUL6h6hQr9HOtuKj1b8JpFrBIjlkcIabl/rT9ndmpDgC9a1oTsin6y6ifNvl2tPvHWPzgxDa4Z3hGGxZeFPocplN/Up0CARg1NZrbNb+1YCJeoMRka//d+bJjxO6ndgpJ7Hhukrp/dqgl/+Hfv6ID2wKE0+019cu8wmv+zVGAe7ctY8jPwaa8qGWdwF2lGPTurH2I//jjtxPQrmscR88xVNO68Jr5GY0Rfih3MR+NnVdc5GcvPkGjutHHrv4+wOz+MKUkj4xOIf",
+      },
+    });
+  
+    return request.json()
+  } catch (error) {
+    
+    console.warn('Não foi possivel obter a lista de mapas tematicos', error)
+  }
+}
+
+function downloadMapaTematico() {
+  const mapaTematico = $("#mapa-tematico-selector").val()
+  window.open(mapaTematico, '_blank')
+}
+
+function downloadCamada() {
+  const camada = $("#camada-selector").val()
+  window.open(camada, '_blank')
+}
+
+$(document).ready(async () => {
+
+  console.log(`lets go`)
+  const listOfCamadas = await getListOfCamadas()
+  console.log("🚀 ~ $ ~ listOfCamadas:", listOfCamadas)
+  const listOfMapasTematicos = await getListOfMapasTematicos()
+  console.log("🚀 ~ $ ~ listOfMapasTematicos:", listOfMapasTematicos)
+
+  listOfMapasTematicos.forEach((mapaTematico) => {
+
+    $(`#mapa-tematico-selector`).append($('<option>', {
+        value: mapaTematico.path,
+        text: mapaTematico.filename
+    }));
+  })
+
+  listOfCamadas.forEach((camada) => {
+
+    $(`#camada-selector`).append($('<option>', {
+        value: camada.path,
+        text: `${camada.filename} (${camada.extension})`
+    }));
+  })
+});
